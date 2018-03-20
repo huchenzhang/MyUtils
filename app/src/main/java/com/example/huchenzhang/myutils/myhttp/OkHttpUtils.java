50189bc6b58b3ca8c530291d3b6f6e85fa56a0eb
huchenzhang <15910699771@163.com>
2018-03-20 12:01:11 +0800

    防抓包处理

--- app/src/main/java/com/example/huchenzhang/myutils/myhttp/OkHttpUtils.java
+++ app/src/main/java/com/example/huchenzhang/myutils/myhttp/OkHttpUtils.java
@@ -3,9 +3,14 @@ package com.example.huchenzhang.myutils.myhttp;
 import android.content.Context;
 import android.util.Log;
 
+import com.example.huchenzhang.myutils.BuildConfig;
 import com.example.huchenzhang.myutils.utils.Constants;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.Proxy;
+import java.net.URL;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -25,6 +30,13 @@ public class OkHttpUtils {
 
 	public static void getAsynHttp(final Context context){
 		mOkHttpClient = new OkHttpClient();
+		//防止被抓包
+		if(!BuildConfig.DEBUG){
+			mOkHttpClient.newBuilder().proxy(Proxy.NO_PROXY);
+		}
+		//或如下方法
+//		URL url = new URL("www.baidu.com");
+//		HttpURLConnection connection = (HttpURLConnection)url.openConnection(Proxy.NO_PROXY);
 		Request.Builder requestBuilder = new Request.Builder().url(BAI_DU_URL);
 		//此处可以省略，默认是GET请求
 		requestBuilder.method(GET,null);
