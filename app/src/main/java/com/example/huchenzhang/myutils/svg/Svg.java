4ddafb9419415072926952cba71ded1a8c26550d
huchenzhang <15910699771@163.com>
2018-03-21 11:07:33 +0800

    练习Glid动图加载

--- app/src/main/java/com/example/huchenzhang/myutils/svg/Svg.java
+++ app/src/main/java/com/example/huchenzhang/myutils/svg/Svg.java
@@ -1,10 +1,13 @@
 package com.example.huchenzhang.myutils.svg;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.bumptech.glide.Glide;
 import com.example.huchenzhang.myutils.BaseActivity;
 import com.example.huchenzhang.myutils.R;
+import com.example.huchenzhang.myutils.databinding.SvgActivityBinding;
 
 /**
  * 测试
@@ -12,9 +15,21 @@ import com.example.huchenzhang.myutils.R;
  */
 
 public class Svg extends BaseActivity {
+	private SvgActivityBinding binding;
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.svg_activity);
+		binding = DataBindingUtil.setContentView(this,R.layout.svg_activity);
+		initGif();
+	}
+
+	/**
+	 * 加载gif图片
+	 */
+	private void initGif(){
+		Glide
+				.with(this)
+				.load(R.mipmap.gif)
+				.into(binding.ivGif);
 	}
 }
